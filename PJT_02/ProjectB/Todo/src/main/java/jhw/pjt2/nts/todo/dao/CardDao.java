98b59d78ef9682d902d667cf3b473710b3d1e381
Hyun Wook Cho <hyunwook_cho@nts-corp.com>
2020-10-30 16:17:04 +0900

    feat : getAllCard 구현
    모든 카드의 정보를 리스트로 받아오는 getAllCard함수를 CardDao에 구현

--- PJT_02/ProjectB/Todo/src/main/java/jhw/pjt2/nts/todo/dao/CardDao.java
+++ PJT_02/ProjectB/Todo/src/main/java/jhw/pjt2/nts/todo/dao/CardDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jhw.pjt2.nts.todo.dto.Card;
 
@@ -14,43 +16,42 @@ public class CardDao {
 	private static final String DB_USER = "user05";
 	private static final String DB_PASSWORD = "user05";
 
-	
 	public int addCard(Card inputCard) {
 		int insertCount = 0;
-		
+
 		Connection conn = null;
 		PreparedStatement ps = null;
-		
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 			String addCardQuery = "insert into card_tb (title, manager_name, priority, column_type) values (?,?,?,?)";
 			ps = conn.prepareStatement(addCardQuery);
-			
+
 			ps.setString(1, inputCard.getTitle());
 			ps.setString(2, inputCard.getManagerName());
 			ps.setInt(3, inputCard.getPriority());
 			ps.setString(4, inputCard.getColumnType());
-			
+
 			insertCount = ps.executeUpdate();
 		} catch (Exception e) {
-			System.out.println("error occured in opening SQLconnection : "+ e);
+			System.out.println("error occured in opening SQLconnection : " + e);
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
 				if (ps != null)
 					ps.close();
 				if (conn != null)
 					conn.close();
 			} catch (SQLException e) {
-				System.out.println("error occured in closing SQLconnection : "+ e);
+				System.out.println("error occured in closing SQLconnection : " + e);
 				e.printStackTrace();
 			}
 		}
-		
+
 		return insertCount;
 	}
-	
+
 	public Card getCardById(Integer cardId) {
 		Card card = null;
 		Connection conn = null;
@@ -70,7 +71,7 @@ public class CardDao {
 						rs.getInt("priority"), rs.getString("registed_date"), rs.getString("column_type"));
 			}
 		} catch (Exception e) {
-			System.out.println("error occured in opening SQLconnection : "+ e);
+			System.out.println("error occured in opening SQLconnection : " + e);
 			e.printStackTrace();
 		} finally {
 			try {
@@ -81,11 +82,42 @@ public class CardDao {
 				if (conn != null)
 					conn.close();
 			} catch (SQLException e) {
-				System.out.println("error occured in closing SQLconnection : "+ e);
+				System.out.println("error occured in closing SQLconnection : " + e);
 				e.printStackTrace();
 			}
 		}
 
 		return card;
+	}
+
+	public List<Card> getAllCard() {
+		List<Card> cardList = new ArrayList<>();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		String getAllCardQuery = "SELECT id, title, manager_name, priority, registed_date, column_type FROM card_tb";
+
+		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+				PreparedStatement ps = conn.prepareStatement(getAllCardQuery)) {
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					Card card = new Card(rs.getInt("id"), rs.getString("title"), rs.getString("manager_name"),
+							rs.getInt("priority"), rs.getString("registed_date"), rs.getString("column_type"));
+					cardList.add(card);
+				}
+			} catch (Exception e) {
+				System.out.println("error occured in ResultSet : " + e);
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			System.out.println("error occured in Connection jdbc: " + e);
+			e.printStackTrace();
+		}
+
+		return cardList;
 	}
 }
--- PJT_02/ProjectB/Todo/src/test/java/APITest.java
+++ PJT_02/ProjectB/Todo/src/test/java/APITest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import jhw.pjt2.nts.todo.dao.CardDao;
 import jhw.pjt2.nts.todo.dto.Card;
 
@@ -5,10 +8,19 @@ public class APITest {
 	public static void main(String[] args) {
 		CardDao dao = new CardDao();
 
-		System.out.println("addCard(testCard1, 조현욱, 1, 'todo')");
-		dao.addCard(new Card("testCard1", "조현욱", 1, "todo"));
+//		System.out.println("addCard(testCard1, 조현욱, 1, 'todo')");
+//		dao.addCard(new Card("testCard1", "조현욱", 1, "todo"));
 		
 		Card result = dao.getCardById(2);
 		System.out.println("getCardById(1)"+result.toString());
+		
+		List<Card> list = new ArrayList<>();
+		list = dao.getAllCard();
+		
+		System.out.println("getAllCard : ");
+		
+		for(Card c : list) {
+			System.out.println(c.toString());
+		}
 	}
 }
