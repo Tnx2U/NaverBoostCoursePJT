f6caa10092f4ef2af384aa6852b9aa5adc5cf1b0
Hyun Wook Cho <hyunwook_cho@nts-corp.com>
2020-10-31 12:26:22 +0900

    feat: main.jsp 생성
    mainServlet에서 포워딩 될 main.jsp파일 생성

--- PJT_02/ProjectB/Todo/src/test/java/APITest.java
+++ PJT_02/ProjectB/Todo/src/test/java/APITest.java
@@ -8,20 +8,20 @@ public class APITest {
 	public static void main(String[] args) {
 		CardDao dao = new CardDao();
 
-		System.out.println("addCard(testCard1, 조현욱, 1, 'todo')");
-		dao.addCard(new Card("testCard1", "조현욱", 1, "todo"));
-		
-		Card result = dao.getCardById(2);
-		System.out.println("getCardById(1)"+result.toString());
-		
-		List<Card> list = new ArrayList<>();
-		list = dao.getAllCard();
-		
-		System.out.println("getAllCard : ");
-		
-		for(Card c : list) {
-			System.out.println(c.toString());
-		}
+//		System.out.println("addCard(testCard1, 조현욱, 1, 'todo')");
+//		dao.addCard(new Card("testCard1", "조현욱", 1, "todo"));
+//		
+//		Card result = dao.getCardById(2);
+//		System.out.println("getCardById(1)"+result.toString());
+//		
+//		List<Card> list = new ArrayList<>();
+//		list = dao.getAllCard();
+//		
+//		System.out.println("getAllCard : ");
+//		
+//		for(Card c : list) {
+//			System.out.println(c.toString());
+//		}
 		
 //		MainServlet mains = new MainServlet();
 //		List<Card> orderedCards[] = mains.getOrderedCards(); 
