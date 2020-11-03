7f649e98b525119797e6fe7cd847d309a0028b87
Hyun Wook Cho <hyunwook_cho@nts-corp.com>
2020-11-03 11:14:55 +0900

    refactor: cardDao, columnDao의 함수들을 try-with-resource 방식으로 변경
    cardDao와 columnDao내부의 모든 함수의 예외처리를 좀 더 간략한
    try-with-resource방식으로 변경

--- PJT_02/ProjectB/Todo/src/main/java/jhw/pjt2/nts/todo/dao/CardDao.java
+++ PJT_02/ProjectB/Todo/src/main/java/jhw/pjt2/nts/todo/dao/CardDao.java
@@ -21,77 +21,27 @@ public class CardDao {
 	public int addCard(Card inputCard) {
 		int insertcount = 0;
 
-		Connection conn = null;
-		PreparedStatement ps = null;
-
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			ps = conn.prepareStatement(QuerySelector.addCardQuery);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
+		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+				PreparedStatement ps = conn.prepareStatement(QuerySelector.addCardQuery)) {
 			ps.setString(1, inputCard.getTitle());
 			ps.setString(2, inputCard.getManagerName());
 			ps.setInt(3, inputCard.getPriority());
 
 			insertcount = ps.executeUpdate();
-			
 		} catch (Exception e) {
 			System.out.println("error occured in opening SQLconnection : " + e);
 			e.printStackTrace();
-		} finally {
-			try {
-				if (ps != null)
-					ps.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				System.out.println("error occured in closing SQLconnection : " + e);
-				e.printStackTrace();
-			}
 		}
 
 		return insertcount;
 	}
 
-	// 번호로 특정 카드 조회
-	public Card getCardById(Integer cardId) {
-		Card card = null;
-		Connection conn = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			ps = conn.prepareStatement(QuerySelector.getCardByIdQuery);
-			ps.setInt(1, cardId);
-			rs = ps.executeQuery();
-
-			if (rs.next()) {
-				card = new Card(rs.getInt("id"), rs.getString("title"), rs.getString("manager_name"),
-						rs.getInt("priority"), rs.getString("registed_date"));
-			}
-		} catch (Exception e) {
-			System.out.println("error occured in opening SQLconnection : " + e);
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-				if (ps != null)
-					ps.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				System.out.println("error occured in closing SQLconnection : " + e);
-				e.printStackTrace();
-			}
-		}
-
-		return card;
-	}
-
-	
 	// 정렬된 전체 카드 조회
 	public List<Card> getOrderedAllCard() {
 		List<Card> cardList = new ArrayList<>();
@@ -104,16 +54,13 @@ public class CardDao {
 
 		// try-with-resource 방식
 		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-				PreparedStatement ps = conn.prepareStatement(QuerySelector.getOrderedAllCardQuery)) {
-			try (ResultSet rs = ps.executeQuery()) {
-				while (rs.next()) {
-					Card card = new Card(rs.getInt("c.id"), rs.getString("c.title"), rs.getString("c.manager_name"),
-							rs.getInt("c.priority"), rs.getString("c.registed_date"), rs.getInt("co.column_id"), rs.getInt("co.card_order"));
-					cardList.add(card);
-				}
-			} catch (Exception e) {
-				System.out.println("error occured in ResultSet : " + e);
-				e.printStackTrace();
+				PreparedStatement ps = conn.prepareStatement(QuerySelector.getOrderedAllCardQuery);
+				ResultSet rs = ps.executeQuery()) {
+			while (rs.next()) {
+				Card card = new Card(rs.getInt("c.id"), rs.getString("c.title"), rs.getString("c.manager_name"),
+						rs.getInt("c.priority"), rs.getString("c.registed_date"), rs.getInt("co.column_id"),
+						rs.getInt("co.card_order"));
+				cardList.add(card);
 			}
 		} catch (Exception e) {
 			System.out.println("error occured in Connection jdbc: " + e);
@@ -122,11 +69,11 @@ public class CardDao {
 
 		return cardList;
 	}
-	
+
 	// 최근 카드 id 조회
 	public int getRecentCardId() {
 		int cardId = -1;
-		
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
@@ -135,14 +82,10 @@ public class CardDao {
 
 		// try-with-resource 방식
 		try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-				PreparedStatement ps = conn.prepareStatement(QuerySelector.getRecentCardIdQuery)) {
-			try (ResultSet rs = ps.executeQuery()) {
-				if(rs.next()) {
-					cardId = rs.getInt(1);
-				}
-			} catch (Exception e) {
-				System.out.println("error occured in ResultSet : " + e);
-				e.printStackTrace();
+				PreparedStatement ps = conn.prepareStatement(QuerySelector.getRecentCardIdQuery);
+				ResultSet rs = ps.executeQuery()) {
+			if (rs.next()) {
+				cardId = rs.getInt(1);
 			}
 		} catch (Exception e) {
 			System.out.println("error occured in Connection jdbc: " + e);
