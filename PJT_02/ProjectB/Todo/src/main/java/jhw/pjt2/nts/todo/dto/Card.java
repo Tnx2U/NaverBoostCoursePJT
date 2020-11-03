d558e4177c10576c78c4a1c1e3eba3f65fd4a41b
Hyun Wook Cho <hyunwook_cho@nts-corp.com>
2020-11-03 12:20:22 +0900

    refactor: 불필요한 card 생성자 삭제

--- PJT_02/ProjectB/Todo/src/main/java/jhw/pjt2/nts/todo/dto/Card.java
+++ PJT_02/ProjectB/Todo/src/main/java/jhw/pjt2/nts/todo/dto/Card.java
@@ -15,17 +15,9 @@ public class Card implements Cloneable {
 		this.title = title;
 		this.managerName = managerName;
 		this.priority = priority;
-		this.registedDate = registedDate.split(":")[0];
+		this.registedDate = registedDate;
 		this.columnId = columnId;
 		this.cardOrder = cardOrder;
-	}
-
-	public Card(int id, String title, String managerName, int priority, String registedDate) {
-		this.id = id;
-		this.title = title;
-		this.managerName = managerName;
-		this.priority = priority;
-		this.registedDate = registedDate;
 	}
 
 	// 자동 생성 파라미터를 제외한 지역변수만 받는 생성자
