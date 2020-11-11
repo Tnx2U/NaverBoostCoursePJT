fd8d28ebf5f0cc3b2f4a17f03c34972fec5e38f9
Hyun Wook Cho <hyunwook_cho@nts-corp.com>
2020-11-11 19:47:27 +0900

    refactor: 커스텀 에러 메시지 추가
    에러 핸들링을 통해 커스텀 에러 메시지 출력
    
    1. 비즈니스 로직이 실행되는 서비스 레이어에서 try-catch로 에러 핸들링
    2. 해당 시점의 파라미터와 호출경로를 포함하는 커스텀 메시지를 출력

--- PJT_03/ProjectC/reservation/src/main/java/chw/intern/nts/reservation/service/impl/ProductServiceImpl.java
+++ PJT_03/ProjectC/reservation/src/main/java/chw/intern/nts/reservation/service/impl/ProductServiceImpl.java
@@ -2,6 +2,10 @@ package chw.intern.nts.reservation.service.impl;
 
 import java.util.List;
 
+import javax.management.RuntimeErrorException;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,31 +16,46 @@ import chw.intern.nts.reservation.service.ProductService;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+	private static final Logger logger = Logger.getLogger(ProductServiceImpl.class);
+
 	@Autowired
 	ProductDao productDao;
 
-	//readonly는 디비마다 메시지를 받아서 다르게 처리
-	@Transactional(readOnly=true)
+	// readonly는 디비마다 메시지를 받아서 다르게 처리
+	@Transactional(readOnly = true)
 	@Override
 	public List<Product> getProductsByCategoryId(Integer categoryId, int start, int limit) {
-		List<Product> productList;
-		if (categoryId == null) {
-			productList = productDao.selectAllWithOffset(start, limit);
-		} else {
-			productList = productDao.selectByCategoryIdWithOffset(categoryId, start, limit);
+		List<Product> productList = null;
+		try {
+			if (categoryId == null) {
+				productList = productDao.selectAllWithOffset(start, limit);
+			} else {
+				productList = productDao.selectByCategoryIdWithOffset(categoryId, start, limit);
+			}
+		} catch (Exception e) {
+			String errorMsg = String.format("Error Occured with params : {categoryId : %d, start : %d, limit: %d}",
+					categoryId, start, limit);
+			System.err.println(errorMsg + e.getLocalizedMessage());
 		}
+
 		return productList;
 	}
 
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	@Override
 	public int getProductsCount(Integer categoryId) {
 		int totalCount = -1;
-		if (categoryId == null) {
-			totalCount = productDao.selectAllCount();
-		} else {
-			totalCount = productDao.selectCountByCategoryId(categoryId);
+		try {
+			if (categoryId == null) {
+				totalCount = productDao.selectAllCount();
+			} else {
+				totalCount = productDao.selectCountByCategoryId(categoryId);
+			}
+		} catch (Exception e) {
+			String errorMsg = String.format("Error Occured with params : {categoryId : %d}", categoryId);
+			System.err.println(errorMsg + e.getLocalizedMessage());
 		}
+
 		return totalCount;
 	}
 }
--- PJT_03/ProjectC/reservation/src/main/java/chw/intern/nts/reservation/service/impl/PromotionServiceImpl.java
+++ PJT_03/ProjectC/reservation/src/main/java/chw/intern/nts/reservation/service/impl/PromotionServiceImpl.java
@@ -1,5 +1,6 @@
 package chw.intern.nts.reservation.service.impl;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import chw.intern.nts.reservation.dao.PromotionDao;
+import chw.intern.nts.reservation.dto.Category;
 import chw.intern.nts.reservation.dto.Promotion;
 import chw.intern.nts.reservation.service.PromotionService;
 
@@ -16,9 +18,16 @@ public class PromotionServiceImpl implements PromotionService {
 	PromotionDao promotionDao;
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public List<Promotion> getAllPromotionWithImgUrl() {
-		List<Promotion> promotionList = promotionDao.selectAllWithImgUrl();
+		List<Promotion> promotionList = Collections.emptyList();
+
+		try {
+			promotionList = promotionDao.selectAllWithImgUrl();
+		} catch (Exception e) {
+			String errorMsg = String.format("Error Occured with params : {}");
+			System.err.println(errorMsg + e.getLocalizedMessage());
+		}
 		return promotionList;
 	}
 }
