fda98aad321c80f6cf62323f3d7a97156b048a40
조현욱 <hyunwook_cho@nts-corp.com>
2020-11-13 17:51:04 +0900

    feat: api/products/{displayInfoId} 컨트롤러 구현
    displayId로 해당 제품의 상세정보를 가져오는 컨트롤러 함수 구현
    
    1. 상세 로직은 구현하지 않고 스켈레톤 코드만 주석으로 구현
    2. CommentService와 ProductService에서 각 로직을 실행시켜 값을 가져오는
       함수 구현

--- PJT_Reservation/ProjectCDEF/reservation/src/main/java/chw/intern/nts/reservation/controller/api/ProductApiController.java
+++ PJT_Reservation/ProjectCDEF/reservation/src/main/java/chw/intern/nts/reservation/controller/api/ProductApiController.java
@@ -22,15 +22,51 @@ public class ProductApiController {
 	ProductService productService;
 
 	@GetMapping
-	public Map<String, Object> productList(@RequestParam(name="categoryId", required=false)Integer categoryId
-	, @RequestParam(name="start", defaultValue="0")int start
-	, @RequestParam(name="limit", required=false, defaultValue="4")int limit){
+	public Map<String, Object> productList(@RequestParam(name = "categoryId", required = false) Integer categoryId,
+			@RequestParam(name = "start", defaultValue = "0") int start,
+			@RequestParam(name = "limit", required = false, defaultValue = "4") int limit) {
 		List<Product> ProductResponse = productService.getProductsByCategoryId(categoryId, start, limit);
 		int totalCount = productService.getProductsCount(categoryId);
 		Map<String, Object> map = new HashMap<>();
 		map.put("items", ProductResponse);
 		map.put("totalCount", totalCount);
 
+		return map;
+	}
+
+	@GetMapping("/{displayInfoId}")
+	public Map<String, Object> productDetailInfo(
+			@RequestParam(name = "displayInfoId", required = true) Integer displayInfoId) {
+		Map<String, Object> map = new HashMap<>();
+//		//필요한 준비
+//		int productId = productService.getProductIdByDisplayInfoId(displayInfoId);
+//		
+//		// averageScore 단일 double
+//		double averageScore = 0;
+//		
+//		// comments 객체 리스트
+//		List<Comment> commentList = commentService.getCommentsByProductId(productId);
+//				
+//		// displayInfo 단일 객체
+//		// 네이밍 컨벤션 질문 (displayInfo, displayInfoResponse)
+//		DisplayInfo displayInfo = productService.getDisplayInfoById(displayInfoId);
+//				
+//		// displayInfoImage 단일 객체
+//		DisplayInfoImage displayInfoImage = productService.getDisplayInfoImageByDisplayInfoId(displayInfoId);
+//		
+//		// productImages 객체 리스트
+//		List<ProductImages> productImageList = productService.getProductImagesByProductId(productId);
+//		
+//		// productsPrices 객체 리스트
+//		List<ProductPrice> productPriceList = productService.getProductPricesByProductId(productId);
+//		
+//		map.put("averageScore", averageScore);
+//		map.put("comments", commentList);
+//		map.put("displayInfo", displayInfo);
+//		map.put("displayInfoImage", displayInfoImage);
+//		map.put("productImages", productImageList);
+//		map.put("productPrices", productPriceList);
+		
 		return map;
 	}
 }
