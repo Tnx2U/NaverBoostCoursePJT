7118165521cc2d2097f34be34c5c90d3523f69b0
조현욱 <hyunwook_cho@nts-corp.com>
2020-11-29 23:22:36 +0900

    feat: session을 사용한 로그인 유지 구현
    상세내용
    1. bookingLogin 화면에서 이메일 입력시 session값에 저장하도록 구현
    2. 상단바를 가진 화면은 jsp에서 reservationEmail을 가지고 있을 시
       예약정보로 넘어가는 a태그를 가지도록 구현
    3. interceptor에서 접근한 컨트롤러와 로그인 여부를 출력하는 로그 구현

--- PJT_Reservation/ProjectCDEF/reservation/src/main/java/chw/intern/nts/reservation/config/WebMvcContextConfiguration.java
+++ PJT_Reservation/ProjectCDEF/reservation/src/main/java/chw/intern/nts/reservation/config/WebMvcContextConfiguration.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import chw.intern.nts.reservation.interceptor.LogInterceptor;
 
 
 @Configuration
@@ -49,10 +52,10 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 		return resolver;
 	}
 
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(new LogInterceptor());
-//	}
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor());
+	}
 //
 //	@Override
 //	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
--- PJT_Reservation/ProjectCDEF/reservation/src/main/java/chw/intern/nts/reservation/interceptor/LogInterceptor.java
+++ PJT_Reservation/ProjectCDEF/reservation/src/main/java/chw/intern/nts/reservation/interceptor/LogInterceptor.java
@@ -2,21 +2,36 @@ package chw.intern.nts.reservation.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter {
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		System.out.println(handler.toString() + " 가 종료되었습니다.  " + modelAndView.getViewName() + "을 view로 사용합니다.");
-	}
 
+	// 컨트롤러가 실행된 후
+//	@Override
+//	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+//			ModelAndView modelAndView) throws Exception {
+//		System.out.println(handler.toString() + " 가 종료되었습니다.  " + modelAndView.getViewName() + "을 view로 사용합니다.");
+//	}
+
+	// 컨트롤러가 실행되기 전
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		HttpSession session = request.getSession();
+		String reservationEmail = (String) session.getAttribute("reservationEmail");
+
 		System.out.println(handler.toString() + " 를 호출했습니다.");
+
+		if (reservationEmail == null) {
+			// 추후 로거로 변경
+			System.out.println("로그인되어 있지 않습니다.");
+		} else {
+			System.out.println("이메일 " + reservationEmail + "로 로그인되어 있습니다.");
+		}
+
 		return true;
 	}
 }
