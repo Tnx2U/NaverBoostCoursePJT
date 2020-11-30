ef1ea36ded1f714f64250f54b55ad34cd4d6492d
조현욱 <hyunwook_cho@nts-corp.com>
2020-11-30 11:02:58 +0900

    refactor: interceptor의 로그 메시지 log4j2를 사용한 logger로 변경
    상세내용
    1. interceptor에서 접근한 컨트롤러와 로그인 여부를 출력하는 메시지를
       logger를 사용해서 출력하도록 변경

--- PJT_Reservation/ProjectCDEF/reservation/src/main/java/chw/intern/nts/reservation/interceptor/LogInterceptor.java
+++ PJT_Reservation/ProjectCDEF/reservation/src/main/java/chw/intern/nts/reservation/interceptor/LogInterceptor.java
@@ -4,11 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogInterceptor.class);
 	// 컨트롤러가 실행된 후
 //	@Override
 //	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
@@ -23,13 +25,12 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
 		HttpSession session = request.getSession();
 		String reservationEmail = (String) session.getAttribute("reservationEmail");
 
-		System.out.println(handler.toString() + " 를 호출했습니다.");
+		LOGGER.info(handler.toString() + " 를 호출했습니다.");
 
 		if (reservationEmail == null) {
-			// 추후 로거로 변경
-			System.out.println("로그인되어 있지 않습니다.");
+			LOGGER.info("로그인되어 있지 않습니다.");
 		} else {
-			System.out.println("이메일 " + reservationEmail + "로 로그인되어 있습니다.");
+			LOGGER.info(String.format("이메일 %s로 로그인되어 있습니다.", reservationEmail));
 		}
 
 		return true;
